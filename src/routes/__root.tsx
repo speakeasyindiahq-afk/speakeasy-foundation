a2005e5d39425f2e04b59abb153b1859cfba1cd4
vivek-oops <aadhya9031@gmail.com>
2026-05-31 13:48:39 +0530

    Add Microsoft Clarity tracking

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -133,6 +133,18 @@ function RootShell({ children }: { children: React.ReactNode }) {
             `,
           }}
         />
+        {/* Microsoft Clarity */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wzmd5rvx8n");
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
